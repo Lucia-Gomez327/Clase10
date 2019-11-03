2826c9efa3f782c63ba8ee1b2f5ee16c6fd5c94d
Apotheosus <apotheosus@outlook.com>
2019-11-03 20:35:54 -0300

    clase Lista_Programas

--- app/src/main/java/com/ulp/clase10/model/Lista_Programas.java
+++ app/src/main/java/com/ulp/clase10/model/Lista_Programas.java
@@ -3,9 +3,57 @@ package com.ulp.clase10.model;
 import java.util.ArrayList;
 
 public class Lista_Programas {
+
     private int count;
+
     private String next;
+
     private String previous;
+
     private ArrayList<Programa> results;
+
+
+    public Lista_Programas(int count, String next, String previous, ArrayList<Programa> results) {
+        this.count = count;
+        this.next = next;
+        this.previous = previous;
+        this.results = results;
+    }
+
+
+    public Lista_Programas() {
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(String previous) {
+        this.previous = previous;
+    }
+
+    public ArrayList<Programa> getResults() {
+        return results;
+    }
+
+    public void setResults(ArrayList<Programa> results) {
+        this.results = results;
+    }
 
 }
